3130c87b04c6bd91f6a8de8cfd7a624fcca34ce7
Umarova Albina <albinaumarova2014@gmail.com>
2022-04-03 00:53:07 +0600

    Level 2

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ---
 
-##Implement a component that draws a nice movie card with a poster, title, and release year of the movie.
+## Implement a component that draws a nice movie card with a poster, title, and release year of the movie.
 
 ---
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
 import { MoviesComponent } from './movies/movies.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieComponent,
-    MoviesComponent
+    MoviesComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
